50e44e699ea060420db656551e3460a9608e4fa4
Alan Duncan <alan@cocoa-factory.com>
2012-10-29 05:47:37 -0500

    Initial Commit

---
+++ CustomAttributeTestApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  CustomAttributeTestApp
+//
+//  Created by alanduncan on 10/29/12.
+//  Copyright (c) 2012 Cocoa Factory, LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
